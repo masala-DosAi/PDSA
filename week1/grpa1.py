804c5d5f4954f5481d160b674ec72bc0ed323bec
masala dosa <jhageee@gmail.com>
2025-01-15 19:41:27 +0530

    Update grpa1.py

--- week1/grpa1.py
+++ week1/grpa1.py
@@ -1,4 +1,4 @@
-#inputs are L&P 
+#inputs are L&P and sadly this code is only passing 4 private tests
 def find_Min_Difference(L,P):
     l=sorted(L)
     sub=[]
@@ -12,4 +12,15 @@ def find_Min_Difference(L,P):
     for t in sub:
         z=max(t)-min(t)
         val.append(z)
-    return sorted(val)[0]
+    return sorted(val)[0]def find_Min_Difference(L,P):
+#approach 2 and this code is the actual answer and passes all 5 private tests
+def find_Min_Difference(L,P):
+  L.sort()
+  N = P
+  M = len(L)
+  min_diff = max(L) - min(L)
+  for i in range(M-N+1):
+    if L[i+N-1] - L[i] < min_diff:
+      min_diff = L[i+N-1] - L[i]
+  return min_diff
+
