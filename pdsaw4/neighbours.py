cf7d0ba42e118fd6b5aa292ff175dbd2bf259adc
masalaDosai <jhageee@gmail.com>
2025-02-09 23:33:43 +0530

    Add files via upload

--- pdsaw4/neighbours.py
+++ pdsaw4/neighbours.py
@@ -1,3 +1,4 @@
+#this code is being used to create a adjancency matrix from the given edges of a graph in the form of a list
 edges = [(1, 2), (1, 3), (2, 4), (3, 4), (4, 5)]
 import numpy as np
 A=np.zeros(shape=(10,10))
@@ -5,6 +6,9 @@ for (i,j) in edges:
     A[i][j]=1
 print(A)
 
+
+
+#this code is being used to identify the neighbours of a given node 
 def neighbours(A,i):
     nbrs=[]
     (rows,cols)=A.shape
@@ -13,4 +17,4 @@ def neighbours(A,i):
             nbrs.append(j)
     return nbrs
 
-print(neighbours(A,1))
+print(neighbours(A,2))
